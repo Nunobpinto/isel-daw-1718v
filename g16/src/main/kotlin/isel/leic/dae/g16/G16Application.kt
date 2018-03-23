f07870efa4c635c2023f2edef481fc1f01d874ed
brunofilipe <bruno.filipe1996@hotmail.com>
2018-03-23 16:36:47 +0000

    setting up project

---
+++ g16/src/main/kotlin/isel/leic/dae/g16/G16Application.kt
@@ -0,0 +1,11 @@
+package isel.leic.dae.g16
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class G16Application
+
+fun main(args: Array<String>) {
+    runApplication<G16Application>(*args)
+}
