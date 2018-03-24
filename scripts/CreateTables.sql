f48afe603e35fbb0f731f53928fcb64808004852
Nunobpinto <nunobernardopinto@gmail.com>
2018-03-24 02:43:15 +0000

    checklist template now allows multiple item templates associated with it

--- scripts/CreateTables.sql
+++ scripts/CreateTables.sql
@@ -1,25 +1,42 @@
-CREATE TABLE template(
-    template_id SERIAL PRIMARY KEY,
-    template_name VARCHAR(50),
-    template_description VARCHAR(50),
-    item_name VARCHAR(50),
-    item_desc VARCHAR(50)
-  );
-
-  CREATE TABLE checklist(
-    checklist_id SERIAL PRIMARY KEY,
-    checklist_name VARCHAR(50),
-    completion_date date,
-    template INTEGER REFERENCES template
-  );
+--------------------------
+-- Auxiliary types
+--------------------------
 
   CREATE TYPE STATE AS ENUM ('Uncompleted', 'Completed');
 
-  CREATE TABLE item (
+--------------------------
+-- Create tables
+--------------------------
+
+  CREATE TABLE checklist_template(
+    checklist_template_id SERIAL,
+    checklist_template_name VARCHAR(50),
+    checklist_template_description VARCHAR(100),
+    PRIMARY KEY (checklist_template_id)
+  );
+
+  CREATE TABLE item_template(
+    item_template_id SERIAL,
+    template_id INTEGER REFERENCES checklist_template,
+    item_template_name VARCHAR(50),
+    item_template_description VARCHAR(100),
+    item_template_state STATE DEFAULT 'Uncompleted',
+    PRIMARY KEY (item_template_id, template_id)
+  );
+
+  CREATE TABLE checklist(
+    checklist_id SERIAL,
+    checklist_name VARCHAR(50),
+    checklist_completion_date date,
+    template_id INTEGER REFERENCES checklist_template,
+    PRIMARY KEY (checklist_id)
+  );
+
+  CREATE TABLE item(
     item_id SERIAL,
     checklist_id INTEGER REFERENCES checklist,
     item_name VARCHAR(50),
-    item_description VARCHAR(50),
-    state STATE,
+    item_description VARCHAR(100),
+    item_state STATE,
     PRIMARY KEY (item_id, checklist_id)
-  )
+  );
--- scripts/DeleteTables.sql
+++ scripts/DeleteTables.sql
@@ -1,3 +1,8 @@
+--------------------------
+-- Deletes table contents
+--------------------------
+
 DELETE FROM item;
-DELETE FROM  checklist;
-DELETE FROM  template;
+DELETE FROM checklist;
+DELETE FROM checklist_template;
+DELETE FROM item_template;
