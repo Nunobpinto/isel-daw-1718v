b6da4419366fb86ec3ed5cf3d8d7207b0b14f959
JpmGameiro <41893@alunos.isel.ipl.pt>
2018-03-25 16:39:44 +0100

    Added drop and delete options to app_user table

--- scripts/DeleteTables.sql
+++ scripts/DeleteTables.sql
@@ -1,7 +1,7 @@
 --------------------------
 -- Deletes table contents
 --------------------------
-
+DELETE FROM app_user;
 DELETE FROM item;
 DELETE FROM checklist;
 DELETE FROM checklist_template;
