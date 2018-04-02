5f13ea8438d4f5d66db115e41c3a61209435418e
brunofilipe <bruno.filipe1996@hotmail.com>
2018-04-02 18:09:32 +0100

    authenticated user registered in checklists and checklist template

--- checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/argumentResolver/ChecklistArgumentResolver.kt
+++ checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/argumentResolver/ChecklistArgumentResolver.kt
@@ -1,12 +1,14 @@
 package isel.leic.daw.checklistsAPI.configuration.argumentResolver
 
 import isel.leic.daw.checklistsAPI.model.Checklist
+import isel.leic.daw.checklistsAPI.model.User
 import org.springframework.core.MethodParameter
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
 import java.time.LocalDate
+import javax.servlet.http.HttpServletRequest
 
 class ChecklistArgumentResolver : HandlerMethodArgumentResolver {
 
@@ -20,9 +22,12 @@ class ChecklistArgumentResolver : HandlerMethodArgumentResolver {
 
         val name: String? = webRequest.getParameter("checklist_name")
         val completionDate: LocalDate? = LocalDate.parse(webRequest.getParameter("completion_date"))
+        val httpServletRequest = webRequest.getNativeRequest(HttpServletRequest::class.java)
+        val username =httpServletRequest!!.userPrincipal.name!!
         return Checklist(
                 checklistName = name,
-                completionDate = completionDate
+                completionDate = completionDate,
+                user= User(username = username)
         )
     }
 
--- checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/argumentResolver/ChecklistTemplateArgumentResolver.kt
+++ checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/argumentResolver/ChecklistTemplateArgumentResolver.kt
@@ -1,11 +1,13 @@
 package isel.leic.daw.checklistsAPI.configuration.argumentResolver
 
 import isel.leic.daw.checklistsAPI.model.ChecklistTemplate
+import isel.leic.daw.checklistsAPI.model.User
 import org.springframework.core.MethodParameter
 import org.springframework.web.bind.support.WebDataBinderFactory
 import org.springframework.web.context.request.NativeWebRequest
 import org.springframework.web.method.support.HandlerMethodArgumentResolver
 import org.springframework.web.method.support.ModelAndViewContainer
+import javax.servlet.http.HttpServletRequest
 
 class ChecklistTemplateArgumentResolver : HandlerMethodArgumentResolver {
 
@@ -19,9 +21,12 @@ class ChecklistTemplateArgumentResolver : HandlerMethodArgumentResolver {
 
         val name: String? = webRequest.getParameter("checklistTemplate_name")
         val description: String? = webRequest.getParameter("checklistTemplate_description")
+        val httpServletRequest = webRequest.getNativeRequest(HttpServletRequest::class.java)
+        val username =httpServletRequest!!.userPrincipal.name!!
         return ChecklistTemplate(
                 checklistTemplateName = name,
-                checklisttemplateDescription = description
+                checklisttemplateDescription = description,
+                user= User(username = username)
         )
     }
 }
