a22c27cc8f6b5a1e62055a2884ab7a9036f63ed9
brunofilipe <bruno.filipe1996@hotmail.com>
2018-06-23 13:47:47 +0100

    rearranged exception handling in filter for OIDC

--- checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/security/OpenIdConnectFilter.kt
+++ checklistsAPI/src/main/kotlin/isel/leic/daw/checklistsAPI/configuration/security/OpenIdConnectFilter.kt
@@ -1,12 +1,17 @@
 package isel.leic.daw.checklistsAPI.configuration.security
 
 import com.fasterxml.jackson.databind.DeserializationFeature
+import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
 import isel.leic.daw.checklistsAPI.exceptions.BadRequestException
 import isel.leic.daw.checklistsAPI.exceptions.UnauthenticatedException
 import isel.leic.daw.checklistsAPI.model.User
+import isel.leic.daw.checklistsAPI.outputModel.error.ErrorOutputModel
 import isel.leic.daw.checklistsAPI.service.UserService
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.MediaType
+import org.springframework.http.ResponseEntity
 import org.springframework.stereotype.Component
 import java.io.BufferedReader
 import java.io.InputStreamReader
@@ -16,6 +21,7 @@ import java.net.URL
 import java.util.*
 import javax.servlet.*
 import javax.servlet.http.HttpServletRequest
+import javax.servlet.http.HttpServletResponse
 
 @Component
 class OpenIdConnectFilter : Filter {
@@ -30,19 +36,43 @@ class OpenIdConnectFilter : Filter {
 
     override fun doFilter(request: ServletRequest, response: ServletResponse, chain: FilterChain) {
         val httpRequest = (request as HttpServletRequest)
+        val httpResponse = (response as HttpServletResponse)
         if( !checkIfProtectedResource(httpRequest) || httpRequest.method == "OPTIONS" ) {
             return chain.doFilter(request, response)
         }
 
-        val bearerToken = getBearerToken(httpRequest) ?: throw BadRequestException()
-        val introspectionResponse = getIntrospectionResponse(bearerToken)
-
-        if( introspectionResponse.active ) {
-            userInfo.sub = introspectionResponse.sub
-            userInfo.user_id = introspectionResponse.user_id
-            return chain.doFilter(request, response)
+        val bearerToken = getBearerToken(httpRequest)
+        if(bearerToken == null){
+            httpResponse.status = HttpStatus.BAD_REQUEST.value()
+            httpResponse.contentType = MediaType.APPLICATION_PROBLEM_JSON_UTF8.toString()
+            val entity =  ResponseEntity(
+                    ErrorOutputModel(
+                            title = "Invalid Syntax",
+                            detail = "Server could not understand the request",
+                            status = 400
+                    ),
+                    HttpStatus.BAD_REQUEST)
+            httpResponse.writer.write(ObjectMapper().writeValueAsString(entity))
         }
-        throw UnauthenticatedException("Unauthorized")
+        else {
+            val introspectionResponse = getIntrospectionResponse(bearerToken)
+
+            if( introspectionResponse.active ) {
+                userInfo.sub = introspectionResponse.sub
+                userInfo.user_id = introspectionResponse.user_id
+                return chain.doFilter(request, response)
+            }
+            httpResponse.status = HttpStatus.UNAUTHORIZED.value()
+            httpResponse.contentType = MediaType.APPLICATION_PROBLEM_JSON_UTF8.toString()
+            val entity =  ResponseEntity(
+                    ErrorOutputModel(
+                            title = "Not Authenticated",
+                            detail = "Authentication Required",
+                            status = 401
+                    ),
+                    HttpStatus.UNAUTHORIZED)
+            httpResponse.writer.write( ObjectMapper().writeValueAsString(entity))
+        }
     }
 
     override fun destroy() {
